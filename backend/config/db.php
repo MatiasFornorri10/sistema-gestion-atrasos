14390fde912cbddebcb1008920701c14bb37bfe8
Thejaimexrz2 <jaimeferrate55@gmail.com>
2025-07-12 01:37:02 -0400

    Arreglado: Conexion valida, funcionando

--- backend/config/db.php
+++ backend/config/db.php
@@ -1,26 +1,30 @@
 ﻿<?php
-// Configuración de la base de datos
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'GestionAtrasosLiceo');
+//El archivo de conexion funciona, se deben cambiar los datos al migrarlo a otro PC o Base de Datos.
+//Comprobado que funcion con el archivo "prueba.php"
+// Configuration de la base de datos
+define('DB_HOST', 'localhost'); 
+define('DB_NAME', 'prueba_db'); //Cambiar por nombre de base de datos actualizado
 define('DB_USER', 'root');
-define('DB_PASS', '224314280');
+define('DB_PASS', '');
+
 date_default_timezone_set('America/Santiago');
 
 // Conexión PDO segura
 function getDB() {
     try {
-        \ = new PDO(
+        $pdo = new PDO(
             "mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=utf8",
-            DB_USER, 
+            DB_USER,
             DB_PASS,
             [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
             ]
+            
         );
-        return \;
-    } catch(PDOException \) {
-        error_log("Error de conexión: " . \->getMessage());
+        return $pdo;
+    } catch(PDOException $e) {
+        error_log("Error de conexión: " . $e->getMessage());
         die("Error al conectar con la base de datos");
     }
 }
